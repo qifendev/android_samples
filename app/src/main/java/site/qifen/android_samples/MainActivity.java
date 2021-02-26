f8ce1b936a2efa8adcf662d9315d2e44afa18c77
qifendev <858810078@qq.com>
2021-02-26 20:32:09 +0800

    add flush and focus and full screen camera

--- app/src/main/java/site/qifen/android_samples/MainActivity.java
+++ app/src/main/java/site/qifen/android_samples/MainActivity.java
@@ -1,12 +1,19 @@
 package site.qifen.android_samples;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.core.Camera;
+import androidx.camera.core.CameraControl;
 import androidx.camera.core.CameraSelector;
+import androidx.camera.core.FocusMeteringAction;
+import androidx.camera.core.FocusMeteringResult;
+import androidx.camera.core.ImageAnalysis;
 import androidx.camera.core.ImageCapture;
+import androidx.camera.core.MeteringPoint;
+import androidx.camera.core.MeteringPointFactory;
 import androidx.camera.core.Preview;
-import androidx.camera.core.impl.PreviewConfig;
+import androidx.camera.core.SurfaceOrientedMeteringPointFactory;
+import androidx.camera.core.UseCaseGroup;
+import androidx.camera.core.ViewPort;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.PreviewView;
 import androidx.core.app.ActivityCompat;
@@ -14,12 +21,23 @@ import androidx.core.content.ContextCompat;
 import androidx.lifecycle.LifecycleOwner;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.util.Log;
+import android.util.Size;
+import android.view.MotionEvent;
+import android.view.OrientationEventListener;
+import android.view.Surface;
+import android.view.View;
 
 import com.google.common.util.concurrent.ListenableFuture;
 
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,6 +45,9 @@ public class MainActivity extends AppCompatActivity {
 
     private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
     private PreviewView previewView;
+
+    private static final int CAMERA_FACING = CameraSelector.LENS_FACING_BACK; //back or front camera
+    private CameraControl cameraControl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,31 +62,122 @@ public class MainActivity extends AppCompatActivity {
 
         previewView = findViewById(R.id.previewView);
 
+        previewView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                touchFocus(event.getX(),event.getY());
+                Log.e("focus", "onTouch focus x: "+event.getX()+" y: "+event.getY() );
+                return false;
+            }
+        });
 
 
         cameraProviderFuture.addListener(() -> {
             try {
+                // Camera provider is now guaranteed to be available
                 ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
-                bindPreview(cameraProvider);
+
+                // Set up the view finder use case to display camera preview
+                Preview preview = new Preview.Builder().build();
+
+                // Set up the capture use case to allow users to take photos
+                imageCapture = new ImageCapture.Builder()
+                        .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+                        .setFlashMode(ImageCapture.FLASH_MODE_ON) //flush on
+                        .build();
+
+
+                // Choose the camera by requiring a lens facing
+                CameraSelector cameraSelector = new CameraSelector.Builder()
+                        .requireLensFacing(CAMERA_FACING)
+                        .build();
+
+                @SuppressLint("UnsafeExperimentalUsageError") ViewPort viewPort = previewView.getViewPort(); //view size
+
+                ImageAnalysis imageAnalysis =
+                        new ImageAnalysis.Builder()
+                                .setTargetResolution(new Size(1280, 720))
+                                .build(); //imageAnalysis
+
+                @SuppressLint("UnsafeExperimentalUsageError") UseCaseGroup useCaseGroup = new UseCaseGroup.Builder()
+                        .addUseCase(preview)
+                        .addUseCase(imageAnalysis)
+                        .addUseCase(imageCapture)
+                        .setViewPort(viewPort)
+                        .build();
+
+                // Attach use cases to the camera with the same lifecycle owner
+                Camera camera = cameraProvider.bindToLifecycle(
+                        ((LifecycleOwner) this),
+                        cameraSelector,
+                        preview,
+                        imageCapture);
+
+
+                cameraControl = camera.getCameraControl();
+
+                // Connect the preview use case to the previewView
+                preview.setSurfaceProvider(
+                        previewView.getSurfaceProvider());
+
+
+
             } catch (ExecutionException | InterruptedException e) {
                 // No errors need to be handled for this Future.
                 // This should never be reached.
             }
         }, ContextCompat.getMainExecutor(this));
 
+
+
+
+
+        OrientationEventListener orientationEventListener = new OrientationEventListener((Context)this) {
+            @Override
+            public void onOrientationChanged(int orientation) {
+                int rotation;
+
+                // Monitors orientation values to determine the target rotation value
+                if (orientation >= 45 && orientation < 135) {
+                    rotation = Surface.ROTATION_270;
+                } else if (orientation >= 135 && orientation < 225) {
+                    rotation = Surface.ROTATION_180;
+                } else if (orientation >= 225 && orientation < 315) {
+                    rotation = Surface.ROTATION_90;
+                } else {
+                    rotation = Surface.ROTATION_0;
+                }
+
+                imageCapture.setTargetRotation(rotation);
+            }
+        };
+
+        orientationEventListener.enable();
+
     }
 
-    void bindPreview(@NonNull ProcessCameraProvider cameraProvider) {
-        Preview preview = new Preview.Builder()
+    void touchFocus(float x,float y){
+        MeteringPointFactory factory = new SurfaceOrientedMeteringPointFactory(previewView.getWidth(), previewView.getHeight());
+        MeteringPoint point = factory.createPoint(x, y);
+        FocusMeteringAction action = new FocusMeteringAction.Builder(point, FocusMeteringAction.FLAG_AF)
+                .addPoint(point, FocusMeteringAction.FLAG_AE) // could have many
+                // auto calling cancelFocusAndMetering in 5 seconds
+                .setAutoCancelDuration(5, TimeUnit.SECONDS)
                 .build();
 
-        CameraSelector cameraSelector = new CameraSelector.Builder()
-                .requireLensFacing(CameraSelector.LENS_FACING_FRONT) //前置摄像头
-                .build();
+        ListenableFuture<FocusMeteringResult> future = cameraControl.startFocusAndMetering(action);
 
-        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+        ExecutorService executor = Executors.newCachedThreadPool();
 
-        Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector, preview);
+        future.addListener( () -> {
+            try {
+                FocusMeteringResult result = future.get();
+                Log.e("camera focus", "touchFocus: "+result.isFocusSuccessful());
+                // process the result
+            } catch (Exception e) {
+            }
+        } , executor);
+
     }
 
 
