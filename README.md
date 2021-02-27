699ea10e1b0db88902a279255fc6f4b89a8c2613
qifendev <858810078@qq.com>
2021-02-27 18:08:11 +0800

    add video capture and readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# 安卓实例
+# 安卓实例-CameraX拍摄照片或者视频
+## 所有拍摄的文件都存放在getFilesDir()方法返回的目录下，viewImage按钮只可以看到图片
+演示：
+![](./images/takePhoto.jpg)
--- app/src/main/java/site/qifen/android_samples/MainActivity.java
+++ app/src/main/java/site/qifen/android_samples/MainActivity.java
@@ -1,7 +1,9 @@
 package site.qifen.android_samples;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.AspectRatio;
 import androidx.camera.core.Camera;
 import androidx.camera.core.CameraControl;
 import androidx.camera.core.CameraSelector;
@@ -16,7 +18,10 @@ import androidx.camera.core.MeteringPointFactory;
 import androidx.camera.core.Preview;
 import androidx.camera.core.SurfaceOrientedMeteringPointFactory;
 import androidx.camera.core.UseCaseGroup;
+import androidx.camera.core.VideoCapture;
 import androidx.camera.core.ViewPort;
+import androidx.camera.core.impl.VideoCaptureConfig;
+import androidx.camera.extensions.BokehImageCaptureExtender;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.PreviewView;
 import androidx.core.app.ActivityCompat;
@@ -27,6 +32,12 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.Size;
@@ -34,6 +45,7 @@ import android.view.MotionEvent;
 import android.view.OrientationEventListener;
 import android.view.Surface;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.TextView;
@@ -46,6 +58,10 @@ import java.io.File;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+
+/**
+ * CameraX图片和视频拍摄，所有拍摄的资源都会存放在getFilesDir方法返回的路径下
+ */
 
 public class MainActivity extends AppCompatActivity {
 
@@ -62,6 +78,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView cameraInfo;
     private SwitchMaterial flushSwitch;
     private SwitchMaterial cameraSwitch;
+    private SwitchMaterial videoRecordSwitch;
     private CameraSelector backCameraSelector;
     private ProcessCameraProvider cameraProvider;
     private Preview preview;
@@ -73,6 +90,7 @@ public class MainActivity extends AppCompatActivity {
     private ListenableFuture<FocusMeteringResult> future;
     private OrientationEventListener orientationEventListener;
     private MeteringPointFactory meteringPointFactory;
+    private VideoCapture videoCapture;
 
 
     @Override
@@ -80,7 +98,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 1);
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO}, 1);
 
         initCamera();
 
@@ -94,6 +112,8 @@ public class MainActivity extends AppCompatActivity {
         flushSwitch = findViewById(R.id.flushSwitch);
 
         cameraSwitch = findViewById(R.id.cameraSwitch);
+
+        videoRecordSwitch = findViewById(R.id.takeVideoSwitch);
 
         findViewById(R.id.viewImage).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -161,13 +181,14 @@ public class MainActivity extends AppCompatActivity {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 cameraProvider.unbindAll();
 
-
                 if (isChecked) {
 
                     camera = cameraProvider.bindToLifecycle(
                             ((LifecycleOwner) MainActivity.this),
                             frontCameraSelector,
-                            useCaseGroup);
+                            preview,
+                            imageCapture,
+                            videoCapture);
 
                     flushSwitch.setEnabled(false);
                     flushSwitch.setText("flush disabled");
@@ -177,7 +198,9 @@ public class MainActivity extends AppCompatActivity {
                     camera = cameraProvider.bindToLifecycle(
                             ((LifecycleOwner) MainActivity.this),
                             backCameraSelector,
-                            useCaseGroup);
+                            preview,
+                            imageCapture,
+                            videoCapture);
 
                     flushSwitch.setEnabled(true);
                     flushSwitch.setText("flush enabled");
@@ -190,15 +213,30 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        videoRecordSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @SuppressLint("RestrictedApi")
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    startRecordVideo();
+                    videoRecordSwitch.setText("video record starting");
+                } else {
+                    videoCapture.stopRecording();
+                    videoRecordSwitch.setText("video record stop");
+                }
+            }
+        });
+
 
     }
 
-    @SuppressLint("UnsafeExperimentalUsageError")
+    @SuppressLint({"UnsafeExperimentalUsageError", "RestrictedApi"})
     private void initCamera() {
 
         executor = Executors.newCachedThreadPool();
 
         cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+
 
         cameraProviderFuture.addListener(() -> {
             try {
@@ -212,6 +250,18 @@ public class MainActivity extends AppCompatActivity {
                 imageCapture = new ImageCapture.Builder()
                         .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY) //image quality
 //                        .setFlashMode(ImageCapture.FLASH_MODE_ON) //flush on
+                        .build();
+
+
+                videoCapture = new VideoCapture.Builder()
+                        //设置宽高比
+//                        .setTargetAspectRatio(AspectRatio.RATIO_16_9)
+                        //分辨率(宽高比和分辨率不能同时设置)
+                        .setTargetResolution(new Size(previewView.getWidth(), previewView.getHeight()))
+                        //视频帧率  越高视频体积越大
+//                        .setVideoFrameRate(25)
+                        //bit率  越大视频体积越大
+//                        .setBitRate(3 * 1024 * 1024)
                         .build();
 
 
@@ -234,6 +284,7 @@ public class MainActivity extends AppCompatActivity {
                 imageAnalysis.setAnalyzer(executor, new ImageAnalysis.Analyzer() { //获取帧
                     @Override
                     public void analyze(@NonNull ImageProxy image) {
+
                         int rotationDegrees = image.getImageInfo().getRotationDegrees();
                         // insert your code here.
                     }
@@ -248,17 +299,18 @@ public class MainActivity extends AppCompatActivity {
                         .setViewPort(viewPort)
                         .build();
 
-//                Camera camera = cameraProvider.bindToLifecycle(
-//                        ((LifecycleOwner) this),
-//                        cameraSelector,
-//                        preview,
-//                        imageCapture);
-
-                // Attach use cases to the camera with the same lifecycle owner
                 camera = cameraProvider.bindToLifecycle(
-                        ((LifecycleOwner) MainActivity.this),
+                        ((LifecycleOwner) this),
                         backCameraSelector,
-                        useCaseGroup);
+                        preview,
+                        imageCapture,
+                        videoCapture);
+
+//                // Attach use cases to the camera with the same lifecycle owner
+//                camera = cameraProvider.bindToLifecycle(
+//                        ((LifecycleOwner) MainActivity.this),
+//                        backCameraSelector,
+//                        useCaseGroup);
 
 
                 cameraControl = camera.getCameraControl();
@@ -290,11 +342,34 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     rotation = Surface.ROTATION_0;
                 }
-                if (imageCapture != null)
-                    imageCapture.setTargetRotation(rotation);
+                if (imageCapture != null) {
+                    preview.setTargetRotation(rotation); //preview rotation
+                    imageCapture.setTargetRotation(rotation); //capture rotation
+                    videoCapture.setTargetRotation(rotation); //video rotation
+                }
             }
         };
         orientationEventListener.enable();
+    }
+
+
+    @SuppressLint("RestrictedApi")
+    void startRecordVideo() {
+        String path = getFilesDir().getAbsolutePath() + "/" + System.currentTimeMillis() + ".mp4";
+        VideoCapture.OutputFileOptions outputFileOptions = new VideoCapture.OutputFileOptions.Builder(new File(path)).build();
+        videoCapture.startRecording(outputFileOptions, executor, new VideoCapture.OnVideoSavedCallback() {
+            @Override
+            public void onVideoSaved(@NonNull VideoCapture.OutputFileResults outputFileResults) {
+//                Uri savedUri = outputFileResults.getSavedUri();
+                t("video saved success");
+            }
+
+            @Override
+            public void onError(int videoCaptureError, @NonNull String message, @Nullable Throwable cause) {
+                t("video saved fail");
+
+            }
+        });
     }
 
 
@@ -341,6 +416,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode != 1) {
+            if (grantResults[0] != PackageManager.PERMISSION_GRANTED && grantResults[1] != PackageManager.PERMISSION_GRANTED) {
+                t("please granted all permission");
+            }
+        }
+
+    }
 }
 
 
